42f43cebe4eb76e2598526bb992bfd8fc1d05898
parasgarg245 <parasgarg245@gmail.com>
2020-12-27 16:38:17 +0530

    solved pattern15

---
+++ pattern/pattern15.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int nspace =n/2 ;
+	int value=1;
+	int ntimes=1;
+
+	for(int i=1;i<=n;i++){
+
+		for(int j=1;j<=nspace;j++)
+				cout<<"\t";
+
+		for(int j=0;j<=ntimes/2 ;j++){
+			cout<<value<<'\t';
+			value++;
+		}
+		--value;
+		for(int j=1;j<=ntimes/2 ;j++){
+			
+			cout<<--value<<'\t';
+		}
+
+
+		// for(int j=0;j<ntimes/2 && i>n/2+1;j++){
+		// 	cout<<--value<<'\t';
+		// }
+
+		// for(int j=0;j<=ntimes/2 && i>n/2+1;j++){
+		// 	value++;
+		// 	cout<<value<<'\t';
+			
+		// }
+		
+
+
+		cout<<endl;
+
+		if(i<=n/2){
+			nspace--;
+			ntimes+=2;
+			value++;
+	
+		}else{
+			nspace++;
+			ntimes-=2;
+			value--;
+		
+		}
+	}
+	
+}
