6032546e3ba0d5c52a1f36ae7310dbe75494b43c
parasgarg245 <parasgarg245@gmail.com>
2021-01-03 14:13:11 +0530

    second.cpp

---
+++ codeforces/long challenge jan/second.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main(){
+
+	char arr[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'};
+
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		int n;
+		cin>>n;
+
+		string s;
+		cin>>s;
+
+		int low=0,high=15;
+		
+		for(int i=0;i<n;i++){
+
+				// int mid=low +(high-low)/2;
+				// cout<<mid<<endl;
+				// int val=s[i]-'0';
+
+				// if(val){
+				// 	low=mid+1;
+				// }else
+				// 	high=mid;
+
+				// if((i!=0 && i%4==0 && low==high) || i==n-1 ){
+				// 	cout<<arr[mid];
+				// 	low=0;
+				// 	high=15;
+					
+				// }
+			int mid=low+(high-low)/2;
+
+			int val=s[i]-'0';
+
+			if(val){
+				low=mid+1;
+			}else{
+				high=mid;
+			}
+
+			if(i%4==3 && low==high ){
+				cout<<arr[low];
+				low=0;
+				high=15;
+			}
+
+
+				
+
+				
+		}
+		cout<<endl;
+	}
+}
