48f511822065cc8326f390f333ea3d419ee34de4
parasgarg245 <parasgarg245@gmail.com>
2021-01-08 17:25:56 +0530

    getkpc.cpp

--- recursion/getkpc.cpp
+++ recursion/getkpc.cpp
@@ -1,44 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string arr[10]={".;", "abc", "def" ,"ghi" ,"jkl" ,"mno","pqrs","tu","vwx","yz"};
+string arr[10] = {
+    ".;",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tu",
+    "vwx",
+    "yz"
+};
 
-vector<string>getkpc(string s){
+vector < string > getkpc(string s) {
+
+    if (s.length() == 0) {
+        vector < string > temp;
+        temp.push_back("");
+
+        return temp;
+    }
 
 
-	vector<string>temp=getkpc(s.substr(1));
+    vector < string > temp = getkpc(s.substr(1));
 
-	int index=s[0]-'0';
+    int index = s[0] - '0';
 
-	string string_at_index=arr[index];
+    string string_at_index = arr[index];
 
-	vector<string>newvec;
+    vector < string > newvec;
 
-	for(int i=0;i<string_at_index.length();i++){
-		
-		for(int j=0;j<temp.size();j++){
+    for (int i = 0; i < string_at_index.length(); i++) {
 
-			arr
-		}
+        char ch = string_at_index[i];
+        for (int j = 0; j < temp.size(); j++) {
 
-	}
+            newvec.push_back(ch + temp[j]);
+        }
 
+    }
+
+    return newvec;
 
 
 }
 
 
-int main(){
+int main() {
 
-	string s;
-	cin>>s;
+    string s;
+    cin >> s;
 
-	vecot<string>ans=getkpc(s);
+    vector < string > ans = getkpc(s);
 
-	int cnt = 0;
+    int cnt = 0;
 
-	  cout << '[';
-    for (string str : ans){
+    cout << '[';
+    for (string str: ans) {
         if (cnt != ans.size() - 1)
             cout << str << ", ";
         else
