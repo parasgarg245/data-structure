0d7defc1abd53e7414d766bded0a60873a21efd5
parasgarg245 <parasgarg245@gmail.com>
2021-01-13 17:03:10 +0530

    printmazewithjump.cpp

--- recursion/printmazewithjump.cpp
+++ recursion/printmazewithjump.cpp
@@ -5,31 +5,30 @@ using namespace std;
     void printMazePaths(int sr, int sc, int dr, int dc, string psf) {
 
     	if(sr==dr && sc==dc){
-
-		cout<<psf<<endl;
-		return;
-	}if(sr>dr || sc>dc){
-		return;
-	}
+    		cout<<psf<<endl;
+    		return;
+    	}if(sr>dr || sc>dc){
+    		return;
+    	}
         
 
-        for(int k=1;k<=dr-sr;k++){
-
-        	string ch=to_string(k);
-
-        	printMazePaths(sr+k,sc,dr,dc,psf+'h'+ch);
-
-
-        }
         for(int k=1;k<=dc-sc;k++){
+
         	string ch=to_string(k);
 
-        	printMazePaths(sr,sc+k,dr,dc,psf+'v'+ch);
+        	printMazePaths(sr,sc+k,dr,dc,psf+"h"+ch);
+
+
         }
-         for(int k=1;k<=dr-sr && k<=dc-sc;k++){
+        for(int k=1;k<=dr-sr;k++){
         	string ch=to_string(k);
 
-        	printMazePaths(sr+k,sc+k,dr,dc,psf+'d'+ch);
+        	printMazePaths(sr+k,sc,dr,dc,psf+"v"+ch);
+        }
+         for(int k=1;(k<=dr-sr) && (k<=dc-sc);k++){
+        	string ch=to_string(k);
+
+        	printMazePaths(sr+k,sc+k,dr,dc,psf+"d"+ch);
         }
         
     }
