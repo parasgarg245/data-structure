074a4e02cfda76063b2f89fccba2dd396d221751
parasgarg245 <parasgarg245@gmail.com>
2021-01-17 13:32:52 +0530

    printdecreasing.cpp

---
+++ recursion/printencoding.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+void printEncoding(string str, string asf){
+    
+
+    
+    
+}
+
+int main(){
+    string str;
+    cin>>str;
+    printEncoding(str,"");
+    
+}
