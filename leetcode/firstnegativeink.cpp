0054fea70d4282013daf63997989a9f68de36cb4
parasgarg245 <parasgarg245@gmail.com>
2021-01-17 13:32:12 +0530

    firstnegativeink.cpp

---
+++ leetcode/firstnegativeink.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int n;
+	    cin>>n;
+	    
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    
+	    int k;
+	    cin>>k;
+	    
+	    int start=0 ,end=0;
+	    
+	    queue<int>q;
+	    
+	    while(end<n){
+		    
+		    if(end-start+1<k){
+		        if(arr[end]<0){
+		            q.push(arr[end]);
+		        }
+		        end++;
+		    }
+		    
+		    if(end-start+1==k){
+		        if(q.empty()){
+		            cout<<0<<" ";
+		        }
+		        
+		        else{
+		            cout<<q.front()<<" ";
+		            
+		            end++;
+		            if(arr[end]<0){
+		                q.push(arr[end]);
+		            }
+		            
+		            if(arr[start]<0){
+		                q.pop();
+		            }
+		            start++;
+		            
+		        }
+		        
+		        
+		    }
+	    }
+	}
+	
+	return 0;
+}
